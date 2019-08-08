0dd014aff65185a693b6175d64ad1ed02d6ac6b5
James Monks <jamesmonks131@gmail.com>
2019-08-08 14:05:42 +1000

    Initialising data prep

--- data-raw/data-prep.R
+++ data-raw/data-prep.R
@@ -4,3 +4,10 @@ ground_truth <- fs::dir_ls(here::here("data-raw", "Z-example-csv")) %>%
   str_subset("ground") %>% 
   vroom::vroom() %>% 
   janitor::clean_names()
+
+player_1 <- fs::dir_ls(here::here("data-raw", "Z-example-csv")) %>% 
+  str_subset("Helios.+_1-moving")
+
+
+fs::dir_ls(here::here("data-raw", "Z-example-csv")) %>% 
+  str_subset("HELIOS.+HELIOS.+_1-moving")
